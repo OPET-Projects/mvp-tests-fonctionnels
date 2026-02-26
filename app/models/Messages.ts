f57e42c15208d4fa1da0f0af7e878d8132ee38c5
olivierfalahi <olivier.falahi@gmail.com>
2026-02-26 16:34:54 +0100

    add initial TypeScript models and enums for application data

---
+++ app/models/Messages.ts
@@ -0,0 +1,7 @@
+export interface Messages {
+    id:string;
+    content:string;
+    createdAt:Date;
+    userId:string;
+    requestId:string;
+}
---
+++ app/models/Resquest.ts
@@ -0,0 +1,8 @@
+import { RequestStatus } from "../enums/RequestStatus";
+
+export interface Request{
+    id:string;
+    status:RequestStatus;
+    articleA:string;
+    articleB:string;
+}
