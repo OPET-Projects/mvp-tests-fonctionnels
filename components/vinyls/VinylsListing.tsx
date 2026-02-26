0ac46b4ab95621c03ff20798b3e05920b6b02f62
Théo Gillet <gillettheo1@gmail.com>
2026-02-26 15:28:05 +0100

    :sparkles: Add vinyls listing page

---
+++ components/vinyls/VinylsListing.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { Vinyl } from '../../lib/types/vinyls';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../ui/card';
+const VinylsListing = ({
+  vinyls
+}: {
+  vinyls: Vinyl[]
+}) => {
+  return (
+    <div className='flex flex-row gap-3 flex-wrap container mx-auto py-10'>
+      {vinyls.map((vinyl) => (
+        <Card key={vinyl.id} className='w-80'>
+          <CardHeader>
+            <CardTitle>{vinyl.name}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{vinyl.description}</p>
+          </CardContent>
+          <CardFooter>
+            <Link href={`/echange/${vinyl.id}`} className='text-sm text-white font-semibold bg-black w-full text-center uppercase px-3 py-2 rounded-sm border border-black hover:bg-white hover:text-black transition'>Échanger</Link>
+
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default VinylsListing;
---
+++ lib/types/vinyls.ts
@@ -0,0 +1,8 @@
+export interface Vinyl {
+  id: number;
+  name: string;
+  description: string;
+  user_id: number;
+  id_request_a: number | null;
+  id_request_b: number | null;
+}
