3178ecdbe81b0bfe12a9ff7a84a452e9cb4c4438
olivierfalahi <olivier.falahi@gmail.com>
2026-02-26 16:35:38 +0100

    add vinyls API routes for CRUD operations

---
+++ app/api/vinyls/user/[id]/route.ts
@@ -0,0 +1,14 @@
+import { connection } from '@/app/services/DbConnector'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest, context: any) {
+    const { id } = await context.params;
+    const sql = connection();
+    try {
+    const vinyls = await sql.query('SELECT * INTO vinyls WHERE user_id = $1', id);
+    return NextResponse.json(vinyls, {status: 200});
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ detail: 'request failed' }, { status: 500 });
+    }
+}
