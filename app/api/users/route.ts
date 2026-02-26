87abc69966dbb28bbedf397cd1405a1a2cd532f7
olivierfalahi <olivier.falahi@gmail.com>
2026-02-26 16:35:23 +0100

    add user API routes for GET and POST requests

---
+++ app/api/users/route.ts
@@ -0,0 +1,14 @@
+import { connection } from '@/app/services/DbConnector'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(request: NextRequest, context: any) {
+    const body = context.body;
+    const sql = connection();
+    try {
+        const user = await sql.query('SELECT name INTO users WHERE code = $1', [body.code])
+        return NextResponse.json(user, { status: 200 });
+    } catch (error) {
+    console.log(error);
+    return NextResponse.json({ detail: 'request failed' }, { status: 500 });
+    }
+}
