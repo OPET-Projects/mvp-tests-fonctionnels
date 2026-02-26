c7cf8e11f12203ce3234b974ce3b04bbc6060dd3
olivierfalahi <olivier.falahi@gmail.com>
2026-02-26 16:36:00 +0100

    add messages API routes for GET and POST requests

---
+++ app/api/messages/[request_id]/route.ts
@@ -0,0 +1,14 @@
+import { connection } from '@/app/services/DbConnector';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest, context: any) {
+    const { request_id } = await context.params;
+    const sql = connection();
+    try {
+      const messages = await sql.query('SELECT * INTO messages WHERE request_id = $1', request_id);
+      return NextResponse.json(messages, {status: 200});
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ detail: 'request failed' }, { status: 500 });
+    }
+}
