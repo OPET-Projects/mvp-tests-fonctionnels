cf9eab7c16a97c86664bdb277e7d642949b9cb43
olivierfalahi <olivier.falahi@gmail.com>
2026-02-27 13:58:56 +0100

    refactor: remove debug log from user vinyls API route

--- app/api/vinyls/user/[id]/route.ts
+++ app/api/vinyls/user/[id]/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
-    console.log('id:', id, typeof id);
     const sql = await connection();
     try {
         const vinyls = await sql.query('SELECT * FROM vinyls WHERE user_id = $1', [parseInt(id)]);
