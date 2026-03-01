e72b32fe1794a3a2824d34e768a874338a6bbe18
olivierfalahi <olivier.falahi@gmail.com>
2026-03-01 15:13:46 +0100

    refactor: enhance VinylsListing layout with header addition and adjusted styling

--- components/vinyls/VinylsListing.tsx
+++ components/vinyls/VinylsListing.tsx
@@ -8,40 +8,44 @@ const VinylsListing = ({
   vinyls: Vinyl[]
 }) => {
   return (
-    <div className='flex flex-row gap-3 flex-wrap container mx-auto py-10'>
-      {vinyls.map((vinyl) => (
-        <Card key={vinyl.id} className='w-80'>
-          <CardHeader>
-            <CardTitle>{vinyl.title}</CardTitle>
-            <CardDescription>{vinyl.artist}</CardDescription>
-          </CardHeader>
-          <CardContent className='flex flex-col items-center justify-center'>
-            {vinyl.fileUrl ?
-              <Image
-              src={vinyl.fileUrl}
-              width={200}
-              height={200}
-              alt={vinyl.title}
-              loading="lazy"
-              /> :
-              <Image
-              src="https://ik.imagekit.io/gits23/placeholder.png"
-              width={200}
-              height={200}
-              alt="Picture of the author"
-              loading="lazy"
-              />
-            }
+      <div className='container mx-auto p-10'>
+        <h1 className='text-3xl font-bold center'>Vinyls</h1>
+          <div className='flex flex-row gap-3 flex-wrap container mx-auto py-10'>
+            {vinyls.map((vinyl) => (
+              <Card key={vinyl.id} className='w-80 justify-between'>
+                <CardHeader>
+                  <CardTitle>{vinyl.title}</CardTitle>
+                  <CardDescription>{vinyl.artist}</CardDescription>
+                </CardHeader>
+                <CardContent className='flex flex-col items-center justify-center'>
+                  {vinyl.file_url ?
+                      <Image
+                          src={vinyl.file_url}
+                          width={200}
+                          height={200}
+                          alt={vinyl.title}
+                          loading="lazy"
+                      /> :
+                      <Image
+                          src="https://ik.imagekit.io/gits23/placeholder.png"
+                          width={200}
+                          height={200}
+                          alt="Picture of the author"
+                          loading="lazy"
+                      />
+                  }
 
-            <CardDescription >{vinyl.description}</CardDescription>
-          </CardContent>
-          <CardFooter>
-            <Link href={`/echange/${vinyl.id}`} className='text-sm text-white font-semibold bg-black w-full text-center uppercase px-3 py-2 rounded-sm border border-black hover:bg-white hover:text-black transition'>Échanger</Link>
+                  <CardDescription>{vinyl.description}</CardDescription>
+                </CardContent>
+                <CardFooter>
+                  <Link href={`/echange/${vinyl.id}`}
+                        className='text-sm text-white font-semibold bg-black w-full text-center uppercase px-3 py-2 rounded-sm border border-black hover:bg-white hover:text-black transition'>Échanger</Link>
 
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+      </div>
   );
 }
 
