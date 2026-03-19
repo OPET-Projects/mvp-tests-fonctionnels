4f6f9af8af1f412c5b804289234f603d2def60be
olivierfalahi <olivier.falahi@gmail.com>
2026-03-19 16:50:33 +0100

    refactor: extract VinylItem component and update VinylsListing and BarterForm to use it

--- components/vinyls/VinylsListing.tsx
+++ components/vinyls/VinylsListing.tsx
@@ -1,14 +1,5 @@
-import Link from 'next/link';
 import { Vinyl } from '@/lib/types/vinyls';
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-} from "../ui/card";
-import Image from 'next/image';
+import VinylItem from "@/components/vinyls/VinylItem";
 const VinylsListing = ({
   vinyls
 }: {
@@ -19,40 +10,7 @@ const VinylsListing = ({
         <h1 className='text-3xl font-bold center'>Vinyls</h1>
           <div className='flex flex-row gap-3 flex-wrap container mx-auto py-10'>
             {vinyls.map((vinyl) => (
-              <Card key={vinyl.id} className='w-80 justify-between'>
-                <CardHeader>
-                  <CardTitle>{vinyl.title}</CardTitle>
-                  <CardDescription>{vinyl.artist}</CardDescription>
-                </CardHeader>
-                <CardContent className='flex flex-col items-center justify-center'>
-                  {vinyl.file_url ?
-                      <Image
-                          src={vinyl.file_url}
-                          width={200}
-                          height={200}
-                          alt={vinyl.title}
-                          loading="lazy"
-                      /> :
-                      <Image
-                          src="https://ik.imagekit.io/gits23/placeholder.png"
-                          width={200}
-                          height={200}
-                          alt="Picture of the author"
-                          loading="lazy"
-                      />
-                  }
-
-                  <CardDescription>{vinyl.description}</CardDescription>
-                </CardContent>
-                <CardFooter>
-                    <Link
-                        href={`/barter/${vinyl.id}`}
-                        className="text-sm text-white font-semibold bg-black w-full text-center uppercase px-3 py-2 rounded-sm border border-black hover:bg-white hover:text-black transition"
-                    >
-                        Échanger
-                    </Link>
-                </CardFooter>
-              </Card>
+              <VinylItem key={vinyl.id} vinyl={vinyl} footer={true} />
             ))}
           </div>
       </div>
