954b1072e1a252ea5a7aedc6bb643dff1430b729
olivierfalahi <olivier.falahi@gmail.com>
2026-03-19 16:50:24 +0100

    refactor: simplify vinyl data fetching in VinylsPage by using VinylsService and userId from localStorage

--- app/vinyls/page.tsx
+++ app/vinyls/page.tsx
@@ -3,45 +3,39 @@
 import VinylsListing from "@/components/vinyls/VinylsListing";
 import {Vinyl} from "@/lib/types/vinyls";
 import {useEffect, useState} from "react";
+import {getAllVinylsByUserId} from "@/services/VinylsService";
 
 export default function VinylsPage() {
-
     const [vinyls, setVinyls] = useState<Vinyl[]>([]);
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState('')
-
-    const loadVinyls = async () => {
-
-        try {
-            const res = await fetch('/api/vinyls', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({id: 4}),
-            });
-            if (!res.ok) {
-                setError('Impossible de charger les vinyls')
-                return
-            }
-            const data = await res.json();
-            setVinyls(data);
-        } catch {
-            setError('Impossible de charger les vinyls')
-        } finally {
-            setLoading(false)
-        }
-    }
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const [userId, setUserId] = useState<number>(0);
 
     useEffect(() => {
-        loadVinyls().then();
-    }, [])
+        const storedId = window.localStorage.getItem("userId") as string;
+        setUserId(Number.parseInt(storedId, 10));
+    }, []);
+
+    useEffect(() => {
+        const fetchVinyls = async () => {
+            try {
+                const data = await getAllVinylsByUserId(userId);
+                setVinyls(data);
+            } catch {
+                setError('Impossible de charger les vinyls');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (userId > 0) {
+            fetchVinyls().then();
+        }
+    }, [userId]);
 
     if (loading) return <p className="p-6 text-gray-600">Chargement…</p>
     if (error) return <p className="p-6 text-red-600">{error}</p>
     if (!vinyls.length) return <p className="p-6 text-gray-600">Aucun vinyle disponible</p>
 
-    return (
-        <VinylsListing vinyls={vinyls}/>
-    );
+    return <VinylsListing vinyls={vinyls}/>;
 }
